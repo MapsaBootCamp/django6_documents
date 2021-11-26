63efd6d175a2d5398915d304d76f2b8fdafe9402
PARSA <parsareef21@gmail.com>
2021-11-26 21:40:14 +0330

    new files and changes added

---
+++ Descriptors.py
@@ -0,0 +1,47 @@
+
+
+
+
+"""
+Descriptors are for contrilling variables
+they are __get__, __set__, __set_name__,  __delet__
+"""
+class One:
+
+    def __set_name__(self, owner, name):
+        self.name = name     
+    """
+    this method get class variables names for using in class One methods
+    """
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    """
+The object is taken by Get and variables can be accessed from inside the object and the 
+property method is also executed
+    """
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise ValueError("you must enter string! ")
+        instance.__dict__[self.name] = value
+
+    """
+the object is taken bt set and variables can be cheacked and set 
+    """        
+
+    def __delete__(self, instance):
+        instance.__dict__[self.name] = None
+
+class Person:
+    name = One()
+    car = One()
+
+    def __init__(self, name, car):
+        self.name = name
+        self.car = car    
+    
+
+p = Person('amir', 'benz')
+p.name = 'parsa'
+p.car = 'hunda'
+print(p.name)
